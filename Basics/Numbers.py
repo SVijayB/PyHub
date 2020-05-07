12cad0ec5c39546ebd121c707e1db6f2deeb9102
vijay <vijaybalajitheboss@gmail.com>
2020-05-07 19:57:40 +0530

    Added Numbers.py

---
+++ Basics/Numbers.py
@@ -0,0 +1,10 @@
+import cmath        #Used for complex numbers.
+
+x = 55.5
+print(int(x))       #Gets only the integer value.
+print(x)
+
+x = 5               #Value of x is reinitialized.
+y = 6
+z = complex(x,y)    #x is the real number and y is the imaginary number.
+print(z)
