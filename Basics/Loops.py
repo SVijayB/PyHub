4dd93cae1914a65133227292092b7c3e8aac5249
vijay <vijaybalajitheboss@gmail.com>
2020-05-07 20:14:11 +0530

    Added loops.py

---
+++ Basics/Loops.py
@@ -0,0 +1,32 @@
+# While Loops : 
+i = 1
+while i<11:
+    print(i)
+    i += 1
+print()
+
+# For Loops : 
+
+#Looping through lists:
+tools = ["Hello","World","!!!"]      
+for x in tools:             #Loops through elements of the list.
+	print(x)
+print()
+
+#Looping through a string:
+for x in "hello":          #Loops through characters in the string.
+    print(x)
+print()
+
+#Range Function:
+for x in range(6):          #0-5
+    print(x)
+print()
+
+for x in range(1, 6):       #1-5
+    print(x)
+print()
+
+for x in range(0, 20, 2):   #0-20, skips 2 -> 2,4,6....18,20
+  print(x)
+print()
