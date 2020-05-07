b0d72517238123a11a2e83b740d2a25fe5d44056
vijay <vijaybalajitheboss@gmail.com>
2020-05-07 20:21:11 +0530

    Added Conditional Statements

---
+++ Basics/CondStatements.py
@@ -0,0 +1,8 @@
+i = int(input("Enter number: "))
+
+if(i<100):
+    print("The number is lesser than 100")
+elif(i==100):
+    print("The number is equal to 100")
+else:
+    print("The number is greater than 100")
