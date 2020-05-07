18ada8fbb3138a31921657de98ef84fc4310d117
vijay <vijaybalajitheboss@gmail.com>
2020-05-07 20:02:49 +0530

    Added variables.py

---
+++ Basics/Variables.py
@@ -0,0 +1,10 @@
+# No specific command to declare a variable.
+
+x = 5           # Automatically assigned when given a value.
+print(x)
+y = 57.15       # Datatype need not be mentioned while initializing.
+print(y)
+x = 54.3        # Data is overwritten if you reassign a new value to the variable.
+print(x)
+X = 52.7        # Variable names are case sensitive.
+print(X)
