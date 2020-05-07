7c7f6ef369292da3417ba3841b4b80f64e114e7d
vijay <vijaybalajitheboss@gmail.com>
2020-05-07 21:34:41 +0530

    Added lists.py

---
+++ Basics/Lists.py
@@ -0,0 +1,20 @@
+print("Lists are a collection of elements. They are mutable. They can have Strings, numbers, Float and more " +
+"or a combination of all of them")
+
+print("For lists we use Sqare brackets.")
+a = [1,2,"Hey!",4,"Wow"]
+print("List A :" , a)
+print()
+
+print("Each element has an unique index. It starts with 0.")
+print()
+
+print("Third element in A :" , a[2])
+a[2] = "Awesome!"
+print("After updating 3rd element in A :", a[2])
+a.insert(0,"Cool")
+print("After adding a new element to A : " , a)
+a.remove("Wow")
+print("After removing the element \"Wow\":", a)
+a.reverse()
+print("After reversing the list :" , a)
