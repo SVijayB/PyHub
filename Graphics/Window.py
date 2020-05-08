f6a9607248ebf306e8006873a66940bea54afdf0
vijay <vijaybalajitheboss@gmail.com>
2020-05-09 01:36:28 +0530

    Added BG to Window

--- Graphics/Window.py
+++ Graphics/Window.py
@@ -1,7 +1,11 @@
 # Creating a window using Tkinter Module.
 from tkinter import *
 window = Tk()
-window.title("Hello World!")
-window.geometry("320x280")
-window.iconbitmap('assets/Graphics/favicon.ico')
-window.mainloop()
+window.title("Hello World!")                            # Setting title of the window.
+window.geometry("350x350")                              # Setting the size of the window.
+window.iconbitmap("assets/Graphics/favicon.ico")        # Setting the icon for the window.
+window.configure(bg="blue")                             # Added Background of the window as blue.
+image = PhotoImage(file="assets/Graphics/Random.PNG")   # Gets Image from location.
+button = Button(window,image=image)                     # Inserts Image as a button in the window.
+button.pack()
+window.mainloop()                                       # Makes sure the window does not autoclose.
