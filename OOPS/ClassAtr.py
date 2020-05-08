5cf1492bbfcf8f5100eda9412aa31bf45ff6e563
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 13:12:42 +0530

    Added dufference between Methods and functions

--- OOPS/ClassAtr.py
+++ OOPS/ClassAtr.py
@@ -13,6 +13,15 @@ class temp:
         b = 5
         print(a+b)
 
-print("\nAccessing function present in class:")
+print("\nAccessing function present in class(Method):")
 a = temp()
 a.sum()
+
+"""
+Difference between Function and Method : Methods are part of a class while functions are not.
+Simply, function and method both look similar as they perform in almost similar way, 
+but the key difference is the concept of 'Class and its Object'.
+Functions can be called only by its name, as it is defined independently. 
+But methods can’t be called by its name only, we need to invoke the class by a reference of that class 
+in which it is defined, i.e. method is defined within a class and hence they are dependent on that class
+"""
