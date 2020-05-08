0552e5f81311dbd1f6b80cb953a252983a1f6f0a
vijay <vijaybalajitheboss@gmail.com>
2020-05-09 01:16:46 +0530

    Added Window.py

---
+++ Graphics/Window.py
@@ -0,0 +1,7 @@
+# Creating a window using Tkinter Module.
+from tkinter import *
+window = Tk()
+window.title("Hello World!")
+window.geometry("320x280")
+window.iconbitmap('assets/favicon.ico')
+window.mainloop()
