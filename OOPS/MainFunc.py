704e3d398d0353cd5dfb3da6fb9969e20cbb0004
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 19:19:55 +0530

    Added MainFunc.py

---
+++ OOPS/MainFunc.py
@@ -0,0 +1,11 @@
+print("\nWe use the main function to manipulate the direction of the flow of the code.")
+
+def world():
+    print("World")
+
+def hello():
+    print("Hello")
+
+if __name__=="__main__":
+    hello()
+    world()
