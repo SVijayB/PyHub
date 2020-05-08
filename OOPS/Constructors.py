13205d3b8da2d880fffd3fad644fd7935d5c0620
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 14:13:46 +0530

    Added Constructors.py

---
+++ OOPS/Constructors.py
@@ -0,0 +1,24 @@
+class temp:
+    def __init__(self):
+        i = 5
+        print("The object has been successfully created")
+
+a = temp()
+
+class temp:
+    def __init__(self,object):
+        i = 5
+        x = "Hello World!"
+        print(object)
+
+a = temp("Object has been successfully created!")
+"""
+A constructor is a special kind of method that python calls when it instantiates an object using 
+the definations found in your class. Python relies on the constructor to perform tasks such as 
+initializing(assigning values to) any instance variables that the object will need when it starts.
+Constructors can also verify that there are enough resources for the object and perform any other 
+start-up task you can think of.
+
+Every class always has a constructor. When you create a class without a constructor, it automatically
+creates a deafult constructor that does nothing.
+"""
