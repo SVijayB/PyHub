2dbe4302e9d3cb4df6bac80e55b39341b5e271d0
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 18:50:00 +0530

    Added DocStrings.py

---
+++ OOPS/DocStrings.py
@@ -0,0 +1,12 @@
+def DocStrings():
+    """
+A python Docstring is a string used to document python a python module, class, function or method,
+so programmers can understand what it does without having to read the details of the implementation.
+A docstring is simple a multi-line string, that is not assigned to anything. It is specified in the source
+code that is used to document a specific segment of code. 
+Docstrings must be defined with three double-quotes. No blank lines should be left before or after the dockstring.
+The text starts in the next line after the opening quotes. 
+The closing quotes have their own line(Not at the end of the last sentence).
+    """
+
+print(DocStrings.__doc__)
