7e056e57f92bfb1494332151185f5992e73859b7
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 19:45:15 +0530

    Added Hierarchial Inheritance

---
+++ OOPS/Inheritance/Hierarchical.py
@@ -0,0 +1,43 @@
+print("\nIn Hierarchial Inheritance, the parent class can be accessed by several Child classes.")
+class parent:
+    def _init_(self):
+        print("Calling Parent Constructor")
+    
+    def parentmethod(self):
+        print("Calling Parent Method")
+
+    def parentmethod2(self):
+        print("Calling the second parent method")
+
+class child(parent):
+    def _init_(self):
+        print("Calling Child Constructor")
+
+    def childmethod(self):
+        print("Calling the Child Method")
+
+    def parentmethod2(self):
+        print("Modified parent method 2")
+
+class child2(parent):       #The parent class can be accessed by several other child class
+    def _init_(self):
+        print("Calling Second Child")
+    
+    def child2method(self):
+        print("Calling the second child's method")
+
+p = parent()
+c = child()
+c2 = child2()
+
+p.parentmethod()        # Accessing Parent Method
+c.childmethod()         # Accessing Child Method
+c.parentmethod()        # Accessing the inherited parent method from child class.  
+p.parentmethod2()       # Accessing the 2nd parent method from parent class
+c.parentmethod2()       # Accessing the 2nd parent method from the child class after modification
+c2.child2method()       # Accessing the 2nd child's method
+c2.parentmethod()       # Accessing the parent class from the 2nd child class.
+
+print("\nView code and read comments for better understanding.")
+
+# Parent class cannot access child class. However, the child class can access the parent class.
