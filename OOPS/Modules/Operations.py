35cca65ea743d0045bacf792a89979e558de60da
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 19:10:38 +0530

    Added Modules and Ignored Pycache

---
+++ OOPS/Modules/Operations.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+def modulas(a,b):
+    return a%b
