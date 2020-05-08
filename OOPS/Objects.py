14d6eb28b8142ff7efbe78804b8dcce32ed4f5f2
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 13:21:43 +0530

    Added Objects.py

---
+++ OOPS/Objects.py
@@ -0,0 +1,9 @@
+print("\nIn python everything is an object. Every initialization and declaration in python is an object.")
+print("Example : ")
+a = 5
+b = "Hey"
+c = 55.5
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(7))
