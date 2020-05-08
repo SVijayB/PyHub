8a9a5ce26531feb0a81fa346d011319588d0d63b
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 14:21:06 +0530

    Documented all Prog in OOPS branch

--- OOPS/ClassAtr.py
+++ OOPS/ClassAtr.py
@@ -16,12 +16,13 @@ class temp:
 print("\nAccessing function present in class(Method):")
 a = temp()
 a.sum()
-
-"""
+def ClassAtr():
+    """
 Difference between Function and Method : Methods are part of a class while functions are not.
 Simply, function and method both look similar as they perform in almost similar way, 
 but the key difference is the concept of 'Class and its Object'.
 Functions can be called only by its name, as it is defined independently. 
 But methods can’t be called by its name only, we need to invoke the class by a reference of that class 
 in which it is defined, i.e. method is defined within a class and hence they are dependent on that class
-"""
+    """
+print(ClassAtr.__doc__)
--- OOPS/Constructors.py
+++ OOPS/Constructors.py
@@ -12,13 +12,16 @@ class temp:
         print(object)
 
 a = temp("Object has been successfully created!")
-"""
-A constructor is a special kind of method that python calls when it instantiates an object using 
-the definations found in your class. Python relies on the constructor to perform tasks such as 
-initializing(assigning values to) any instance variables that the object will need when it starts.
-Constructors can also verify that there are enough resources for the object and perform any other 
-start-up task you can think of.
 
-Every class always has a constructor. When you create a class without a constructor, it automatically
-creates a deafult constructor that does nothing.
-"""
+def constructors():
+    """
+    A constructor is a special kind of method that python calls when it instantiates an object using 
+    the definations found in your class. Python relies on the constructor to perform tasks such as 
+    initializing(assigning values to) any instance variables that the object will need when it starts.
+    Constructors can also verify that there are enough resources for the object and perform any other 
+    start-up task you can think of.
+
+    Every class always has a constructor. When you create a class without a constructor, it automatically
+    creates a deafult constructor that does nothing.
+    """
+print(constructors.__doc__)
