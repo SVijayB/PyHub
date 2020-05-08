3efe1bab86012d0c713e4845831441d5f521e20b
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 14:27:30 +0530

    Documented Operators.py

--- Basics/Operators.py
+++ Basics/Operators.py
@@ -4,21 +4,23 @@ print("X :", x)
 x += 5
 print("X + 5 =" , x)
 
-"""
+def SimOperators():
+	"""
 Similar Operators : 
 	+=
 	-=
 	/=
 	x=
 	%=
-	**= 		#Multiplies the variable by the number of times, eg : x = 5; x**=3 ans is 125
-	//=  		#Floor Divison
-	&=			#And operation
-	|=   		#Or Operation
+	**= 			#Multiplies the variable by the number of times, eg : x = 5; x**=3 ans is 125
+	//=  			#Floor Divison
+	&=				#And operation
+	|=   			#Or Operation
 	^=
 	>>= Right Shift
 	<<= Left Shift
-"""
+	"""
+print("\n",SimOperators.__doc__)
 
 y = 10
 print("Y :" , y)
@@ -27,24 +29,29 @@ y = 5
 print("Y :",y)
 print("Is X = Y ?" , (x is y))
 
-"""
+def SimOperators1():
+	"""
 Similarly,
     Identity Operators : 
-	is			#Checks whether both the operands refer to the same object or not.
-	is not		#Opposite of is operator.
+	is						#Checks whether both the operands refer to the same object or not.
+	is not					#Opposite of is operator.
 
     Membership Operators : 
-	    in			#Returns true if specified value is present in the object, else returns false.
-	    Not in		#Opposite of in operator.
-"""
+	    in					#Returns true if specified value is present in the object, else returns false.
+	    Not in				#Opposite of in operator.
+	"""
+print("\n",SimOperators1.__doc__)
 
+print("X :", x)
+print("Y :" , y)
 if(x==y):
     print("X is equal to Y")
 
 if(x!=y):
     print("X is not equal to Y")
 
-"""
+def SimOperators2():
+	"""
 Comprison Operators : 
 	==
 	!=
@@ -58,4 +65,5 @@ Logical Operators :
 	&	#Logical And
 	|	#Logical Or
 	~	#Logical Not
-"""
+	"""
+print("\n",SimOperators2.__doc__)
