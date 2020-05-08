4176496d7fa8caf68bc256ab12bf65cf0ad95542
vijay <vijaybalajitheboss@gmail.com>
2020-05-08 10:52:55 +0530

    Added Partial functions to Functions.py

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -1,4 +1,5 @@
 import functools
+from functools import partial
 
 print("To create functions :\n"+"def functionname(): \n\tfunctionbody")
 print()
@@ -10,15 +11,27 @@ print("Without default arguments :" , full_name())
 print("With Default arguments :" , full_name(last="S"))
 print()
 
-print("Unpacking Arguments : ")
+print("Unpacking Arguments :")
 def selfie(name,age,job) : 
 	print("", name , "\n" , age , "\n" , job)
 data = ["Vijay",18,"Nothing"]
 selfie(*data)
 print()
 
-print("Reducing Functions : ")
+print("Reducing Functions :")
 a = [1,2,3,4,5,6,7]
 print("List A :",a)
 print("Sum of all elements in A :" ,        #To print in the same line
 functools.reduce(lambda a,b : a+b, a))      #Have to import functools to reduce. 
+print()
+
+print("Partial Functions :")
+
+def fun(a,b,c,d):
+	return a+b+c+d
+
+print("Default Argument passing :" , fun(1,2,3,4))
+par = partial(fun,5,6,7)					#Have to import from functools to access.
+print("Using Partial Function :" , par(7))
+par = partial(fun,6,7)
+print("Changing two arguments : " , par(8,9))
