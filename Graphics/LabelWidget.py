682b381a040c9e9906f65c2ba5565b4dc3f5b03a
vijay <vijaybalajitheboss@gmail.com>
2020-05-09 09:35:17 +0530

    Added Buttons to LabelWidget.py

--- Graphics/LabelWidget.py
+++ Graphics/LabelWidget.py
@@ -7,4 +7,6 @@ tk.geometry("500x500")
 tk.iconbitmap("assets/Graphics/favicon.ico")
 label = Label(tk,text="I am 1StranGe",font=("Arial Bold",18))      
 label.grid(column=0,row=0)              # Setting the string to print at pos (0,0)
+button  = Button(tk,text="Click here!",bg="black",fg="white")           # Background and font colours.
+button.grid(column=1,row=0)             # Used to place the button on the window.
 tk.mainloop()
