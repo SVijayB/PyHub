82bc03fa6d4eb9bcd69366cff4eb753bd7372bfd
vijay <vijaybalajitheboss@gmail.com>
2020-05-10 21:58:22 +0530

    Added Else block

---
+++ Errors_and_Exceptions/Else Block.py
@@ -0,0 +1,15 @@
+try:
+    age = int(input("Enter your age : "))
+except ValueError:
+    print("VALUE ERROR: ENTER ONLY INTEGER NUMBERS")
+else:
+    print("You are",age,"Years old")
+    try:
+        exp = int(input("How many years have you been coding?"))
+    except:
+        print("VALUE ERROR: ENTER YEARS IN NUMBERS ONLY")
+    else:
+        if(age<15 and exp>=1):
+            print("Awesome! You are only",age,"and you've already coded for",exp,"years long!")
+        else:
+            print("You have a coding experiance of",exp,"years")
