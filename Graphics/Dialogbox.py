b357609dfcdfced660491ff02fe8f67ccba9b830
vijay <vijaybalajitheboss@gmail.com>
2020-05-10 11:07:03 +0530

    Added Dialog box

---
+++ Graphics/Dialogbox.py
@@ -0,0 +1,20 @@
+from tkinter import * 
+from tkinter import simpledialog
+
+window = Tk()
+window.title("Check Boxes")
+window.geometry("320x240")
+window.iconbitmap("assets/Graphics/favicon.ico")
+colour = simpledialog.askstring("Colour","Enter A Colour : ",parent=window) #First arg = title, second = message
+if colour == None:
+    print("You did not enter any colour :(")
+else:
+    print("You have entered",colour)
+
+number = simpledialog.askinteger("Number","Enter A Number : ",parent=window) #askinteger to take only integer type values
+if number == None:
+    print("You did not enter any number :(")
+else:
+    print("You have entered",number)
+
+window.mainloop()
