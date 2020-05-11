7f2a79c5497ac67000fa5fba7321f3a4f8a36985
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 09:04:10 +0530

    Added erorr exception to print docStrings

--- File Handling/Renaming a file.py
+++ File Handling/Renaming a file.py
@@ -1,6 +1,13 @@
 # Renaming a file using the OS module.
 import os
 
-os.rename("assets\File Handling\Random.txt","assets\File Handling\Message.txt")
-# The rename function takes two arguments, the first is the path to the file you want to change the name.
-# Second is the name of the file you want to change it to(Make sure you add path as well).
+try:
+    os.rename("assets\File Handling\Random.txt","assets\File Handling\Message.txt")
+except FileNotFoundError:
+    print("\nERROR : File does not exist! Check your path or the file name once again")
+def doc():
+    """
+The rename function takes two arguments, the first is the path to the file you want to change the name.
+Second is the name of the file you want to change it to(Make sure you add path as well).
+    """
+print(doc.__doc__)
