cc2ba0e7acd508501f1dd9bfe129203c022735d4
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 09:01:34 +0530

    Renaming file using the OS module

---
+++ File Handling/Renaming a file.py
@@ -0,0 +1,6 @@
+# Renaming a file using the OS module.
+import os
+
+os.rename("assets\File Handling\Random.txt","assets\File Handling\Message.txt")
+# The rename function takes two arguments, the first is the path to the file you want to change the name.
+# Second is the name of the file you want to change it to(Make sure you add path as well).
