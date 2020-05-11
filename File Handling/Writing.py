70103e46c047a4ce64e8c2024e406b4864264014
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 09:11:05 +0530

    Writing to a file

---
+++ File Handling/Writing.py
@@ -0,0 +1,6 @@
+# Writing to a file.
+path = "assets/File Handling/Message.txt"
+
+file = open(path,"w")
+file.write("Hey! This is 1StranGe!")
+file.write(" I am working on file handling in python.")
