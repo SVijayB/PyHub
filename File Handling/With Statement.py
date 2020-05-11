139a78487a67d0269c8750bf9678598e2d4800e9
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 09:35:56 +0530

    Added With Statement

---
+++ File Handling/With Statement.py
@@ -0,0 +1,14 @@
+def doc():    
+    """
+Using the with statement, you can get better syntax and exception handling. 
+The with statement simplifies exception handling by encapsulating common preparation and cleans up tasks.
+File is automatically closed using the with statement.
+    """
+print(doc.__doc__)
+
+path = "assets\File Handling\Random.txt"
+
+with open(path,"a") as file:
+    file.write("\nThis message is appended using the with statement")
+
+# file.write("With statement!") if you run this line, it would show errors as the with statement closes the file automatically.
