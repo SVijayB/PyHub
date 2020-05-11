a5b13a103bb2cfcc8353e2b479a004b4700d413e
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 08:48:59 +0530

    Added File.py

---
+++ File Handling/File.py
@@ -0,0 +1,4 @@
+# Creating a new text file.
+path = "assets/File Handling/Random.txt"        # Remember to enter the file name you want to create.
+print("Creating a file at assets/File Handling with the name Random.txt")
+file = open(path, "w+")    
