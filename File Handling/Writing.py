9853652cdd15fb29d1adf2574b0317ecb1942679
vijay <vijaybalajitheboss@gmail.com>
2020-05-11 09:21:00 +0530

    Added Append.py

--- File Handling/Writing.py
+++ File Handling/Writing.py
@@ -4,3 +4,5 @@ path = "assets/File Handling/Message.txt"
 file = open(path,"w")
 file.write("Hey! This is 1StranGe!")
 file.write(" I am working on file handling in python.")
+
+print("\nNote : Data is automatically saved in the file when the program is run.")
