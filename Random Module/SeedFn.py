8cbdd533e7122e3e52f27159106e1808036d6e5c
vijay <vijaybalajitheboss@gmail.com>
2020-05-12 10:59:31 +0530

    Added Seed function

---
+++ Random Module/SeedFn.py
@@ -0,0 +1,6 @@
+import random
+# If you use seed function, the values generated for the same number remains same.
+i = int(input("Enter a number : "))
+x = random.seed(i)
+y = random.random()
+print(y)
