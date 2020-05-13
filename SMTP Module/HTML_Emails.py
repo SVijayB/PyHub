33ced588cfcf3149218704de95555c08c7b2e871
vijay <vijaybalajitheboss@gmail.com>
2020-05-13 14:24:53 +0530

    Completed code and fixed minor bugs

--- SMTP Module/HTML_Emails.py
+++ SMTP Module/HTML_Emails.py
@@ -6,9 +6,8 @@ host = "smtp.gmail.com"
 port = 587                  
 
 username = str(input("Enter Gmail Username : "))                                
-password = str(input ("Enter Gmail Password"))                                           
-message = "Hey there! I sent this MAIL using Python's SMTP module"  
-_to = [username]            # Add any other username you want by adding a comma.                
+password = str(input ("Enter Gmail Password : "))                                           
+_to = username            # Add any other username you want by adding a comma.                
 _from = username
 try:
     connection = smtplib.SMTP(host,port)
@@ -16,12 +15,27 @@ try:
     connection.starttls()
     connection.login(username,password)         
     print("\nCONNECTION SUCCESSFUL")
-    connection.sendmail(_from,_to,message)      
+
+    tmessage = MIMEMultipart("alternate")           # For the mail to understand HTML
+    tmessage ["Subject"] = "Html Message"           # You can add Subjects as well.
+    tmessage["From"] = _from
+    tmessage["To"] = _to
+    
+    html_message =  """
+    <html>
+        <body>
+            <h1>HTML MESSAGE</h1>
+            <p>Hey! This is a HTML Message</p>
+        </body>
+    </html>
+                    """
+    msg = MIMEText(html_message,"html")
+    tmessage.attach(msg)
+
+    connection.sendmail(_from,_to,tmessage.as_string())      
     print("Message has been sent Successfully")
     connection.quit()                           
     print("Connection has been closed.")
-    tmessage = MIMEMultipart("alternate")           # For the mail to understand HTML
-    tmessage ["Subject"] = "Html Message"           # You can add Subjects as well.
-    message[]
 except:
     print("CONNECTION ERROR")
+    print("PROGRAM TERMINATED")
