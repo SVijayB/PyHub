0fb89e1df048e806e0f3953eb4613a1d7f3a2927
vijay <vijaybalajitheboss@gmail.com>
2020-05-14 18:11:53 +0530

    Traversing through SLL

--- Linked List/Single Linked List.py
+++ Linked List/Single Linked List.py
@@ -1,20 +1,28 @@
 class Node:
-    def __init__(self,datavalue=None):
-        self.data = datavalue
-        self.nextvalue = None
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
 
 class LinkedList : 
     def __init__(self):
-        self.headvalue = None
+        self.head = None
+    
+    def printing(self):
+        temp = self.head
+        while(temp != None):
+            print(temp.data)
+            temp = temp.next
 
 # Creation 
 x = LinkedList()
-x.headvalue = Node("Red")       # First node
+x.head = Node("Red")            # First node
 data2 = Node("Green")           # Filling Data Values for each node.
 data3 = Node("Blue")            
 
-x.headvalue.nextvalue = data2   # Head now points to data2
-data2.nextvalue = data3         # data2 now points to data3
+x.head.next = data2             # Head now points to data2
+data2.next = data3              # data2 now points to data3
 
+print()
+x.printing()
 
 
