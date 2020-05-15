c0468aa22c8e8975677887293f432ad0f5fa03aa
vijay <vijaybalajitheboss@gmail.com>
2020-05-15 12:51:30 +0530

    Added Insertion for Doubly Linked Lists

---
+++ Linked List/Doubly Linked List/Insertion.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+
+# Inserting at the beginning : 
+    def add(self,data):
+        temp = Node(data)
+        temp.next = self.head
+        if(self.head!=None):
+            self.head.prev = temp
+        self.head = temp 
+
+# Inserting after a Node :
+    def insertion(self,node,data):
+        temp = Node(data)
+        temp.next = node.next
+        node.next = temp
+        temp.prev = node
+        if (temp.next!=None): 
+            temp.next.prev = temp
+
+# Inserting at the last Node:
+    def end(self,data):
+        new = Node(data)
+        temp = self.head
+        new.next = None
+        if (self.head == None):
+            new.prev = None
+            self.head = new
+            return
+        while(temp.next != None):
+            temp = temp.next
+        temp.next = new
+        new.prev = temp 
+
+    def printing(self):
+        temp = self.head
+        while(temp!=None):
+            print(temp.data)
+            temp = temp.next
+
+dll = DoubleLinkedList
+dll.add("Red")
+dll.add("Yellow")
+dll.insertion(dll.head.next,"Red")
+dll.end("Green")
+dll.printing()
