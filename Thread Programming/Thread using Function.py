8ac7d29f0fb50cc6b9a993d3df34e97d876f88b5
vijay <vijaybalajitheboss@gmail.com>
2020-05-16 14:59:51 +0530

    Added Thread names

--- Thread Programming/Thread using Function.py
+++ Thread Programming/Thread using Function.py
@@ -2,9 +2,14 @@ from threading import Thread
 import threading
 
 def evenNumber():
+    a = threading.current_thread().getName()
+    print("Thread Inside the function :",a)
     for x in range (20):
         if x%2 == 0:
             print(x)
 
+a = threading.current_thread().getName()
+print("\nThread outside the function : "+a)
 t = Thread(target=evenNumber)
 t.start()
+
