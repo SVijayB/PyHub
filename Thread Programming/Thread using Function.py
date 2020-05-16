7d68b47a94e6e24d9b99b282d9d7ce00e1b1bdcb
vijay <vijaybalajitheboss@gmail.com>
2020-05-16 14:53:10 +0530

    Fixed threading not starting

--- Thread Programming/Thread using Function.py
+++ Thread Programming/Thread using Function.py
@@ -1,9 +1,10 @@
 from threading import Thread
+import threading
 
 def evenNumber():
     for x in range (20):
-        if(x%2==0):
+        if x%2 == 0:
             print(x)
-    
+
 t = Thread(target=evenNumber)
-t.start
+t.start()
