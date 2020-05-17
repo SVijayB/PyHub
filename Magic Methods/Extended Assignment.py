8b1498545b8d296401ff4e753ceb9e6f09455f47
vijay <vijaybalajitheboss@gmail.com>
2020-05-17 18:42:16 +0530

    Added extended assignment doc

---
+++ Magic Methods/Extended Assignment.py
@@ -0,0 +1,20 @@
+# Extended Assignment Operators : 
+def doc():
+    """
+Types of binary operators : 
+opr         Magic                       Job
++=       __iadd__(self,others)          Add
+-=       __isub__(self,others)          Sub
+*=       __imul__(self,others)          Mul
+/=       __idiv__(self,others)          Div
+//=      __ifloordiv__(self,others)     Floor
+%=       __imod__(self,others)          Mod
+**=      __ipow__(self,others)          Power
+<<=      __ilshift__(self,others)       Left Shift
+>>=      __irshift__(self,others)       Right Shift
+&=       __iand__(self,others)          AND
+^=       __ixor__(self,others)          XOR
+|=       __ior__(self,others)           OR
+    """
+print(doc.__doc__)
+print("EXAMPLE : ")
