fbb5f02c1231c3b0c7dab4ee90a7c2aa8ba0680b
vijay <vijaybalajitheboss@gmail.com>
2020-05-17 17:49:00 +0530

    added Binary operator overlaoding

--- Magic Methods/Binary Operators.py
+++ Magic Methods/Binary Operators.py
@@ -17,3 +17,25 @@ $       __and__(self,others)        AND
 |       __or__(self,others)         OR
     """
 print(doc.__doc__)
+print("EXAMPLE : ")
+class operators:
+    def __init__(self, *arguments):
+        if (len(arguments)==0):
+            self.numbers = (0,0)
+        else:
+            self.numbers = arguments
+    
+    def __add__(self,other):
+        sum = tuple(x + y for x,y in zip(self.numbers, other.numbers))      # Vector Addition
+        return operators(*sum)
+
+    def __mul__(self,other):
+        mul = tuple(x * y for x,y in zip(self.numbers, other.numbers))      # Vector Multiplication
+        return operators(*mul)
+    
+a = operators(2,3)
+b = operators(5,6)
+c = a + b       # __add__ is automatically called
+d = a * b       # __mul__ is automatically called
+print(c.numbers)
+print(d.numbers)
