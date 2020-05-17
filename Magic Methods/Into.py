4df209eecfe69a1e8c928996e0d4f00ce0c1b27b
vijay <vijaybalajitheboss@gmail.com>
2020-05-17 15:18:58 +0530

    Added Intro to magic methods

---
+++ Magic Methods/Into.py
@@ -0,0 +1,18 @@
+def doc():
+    """
+The magic methods in python are like normal methods but with double underscore before and after the method name.
+These double underscores are called dunder. These methods are called dunder or magic methods.
+A constructor function like init is also a magic method.
+Example :
+    """
+print(doc.__doc__)
+class x:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    
+    def sum(self):
+        return self.a + self.b
+    
+obj = x(3,4)
+print("Sum is =",obj.sum())
