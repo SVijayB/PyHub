3e29e8affea16647e6bc09a000a41be897cab956
vijay <vijaybalajitheboss@gmail.com>
2020-05-17 22:00:43 +0530

    Completed length convertor

--- Magic Methods/repr method.py
+++ Magic Methods/repr method.py
@@ -2,24 +2,26 @@ class measurement:
     value = {"mm":0.001, "cm":0.01, "m":1 , "km":1000, "in":0.0254,
          "ft":0.3048, "yd":0.9144, "mi":1609.344}   # Converting to meters.
         
-    def __init__(self, x, value_unit="m"):
+    def __init__(self, x, unit="m"):
         self.x = x
-        self.value_unit = value_unit
+        self.unit = unit
     
     def convertor(self):
-        return self.x * measurement.value[self.value_unit]
+        return self.x * measurement.value[self.unit]
 
     def __add__(self,other):
         ans = self.convertor() + other.convertor()
-        return measurement(ans/measurement.value[self.value_unit],self.value_unit)
+        return measurement(ans)
     
     def __str__(self):      # Converts into string
         return str(self.convertor)  
     
     def __repr__(self):     # Evaluates -> representation
-        return "After Convertion : " + str(self.x) + " , " + self.value_unit
+        return "After Convertion : " + str(self.x) + "m"
 
 if __name__ == "__main__":
-    obj = measurement(5.5, "yd") + measurement(1)
-    print(repr(obj))
-    print(obj)
+    a = measurement(5.5, "yd") 
+    b = measurement(5.5, "in") 
+    obj = a+b
+    print(repr(obj))        # For printing the representation of the answer
+    # print(obj)            # For printing the object reference
