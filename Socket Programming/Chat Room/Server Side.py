b44ba916dd31ea7d05245079eb7d5b5544af9008
vijay <vijaybalajitheboss@gmail.com>
2020-05-18 14:48:13 +0530

    Completed Accepting clients

--- Socket Programming/Chat Room/Server Side.py
+++ Socket Programming/Chat Room/Server Side.py
@@ -11,6 +11,10 @@ def accept_client():
     while (True):
         client_con,client_address=s.accept()
         print(client_address, "Has Connected")
+        client_con.send("Hey! Welcome to the Chat Room. Enter Your Name To Continue.".encode("utf8"))
+        addresses[client_address] = client_address
+        t2 = Thread(target=handle_client,args=(client_con,client_address))
+        t2.start()
 
 if __name__ == "__main__":
     s.listen(5)         # Only 5 clients can join
