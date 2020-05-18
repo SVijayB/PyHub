b3963fe35d8c8dfe3edfb7ff14f6eafd8dbb2dc1
vijay <vijaybalajitheboss@gmail.com>
2020-05-18 15:28:46 +0530

    Created a Structure for the Client side

--- Socket Programming/Chat Room/Client Side.py
+++ Socket Programming/Chat Room/Client Side.py
@@ -5,18 +5,39 @@ from threading import Thread
 
 window = Tk()
 window.title("Chat Room")
-window.configure(bg="black")
+window.configure(bg="white")
 
-message_frame = Frame(window,height=100,width=100,bg="white")
+message_frame = Frame(window,height=100,width=100,bg="black")
 
 my_message = StringVar()
 my_message.set("")
 
 scroll_bar = Scrollbar(message_frame)
 
-message_list = Listbox(message_frame,height = 15,width = 100,bg = "white", yscrollcommand=scroll_bar.set)
+message_list = Listbox(message_frame,height = 15,width = 100,bg = "black", yscrollcommand=scroll_bar.set)
 
 scroll_bar.pack(side=RIGHT,fill=Y)
 message_list.pack(side=LEFT,fill=BOTH)
 message_frame.pack()
+
+button_label = Label(window,text = "Enter Your Message",fg="black",bg="white")
+button_label.pack()
+
+text_field = Entry(window,textvariable=my_message, fg="black", bg="white",width = 50)
+text_field.pack()
+
+send_button = Button(window,text = "Send", bg="white",fg="black", command = send)
+send_button.pack()
+
+quit_button = Button(window,text = "Exit", bg="white",fg="black", command = closing)
+quit_button.pack()
+
+window.protocol("WM_DELETE_WINDOW",closing)
+
+host = "192.168.0.192"; port = 8080
+s = socket.socket()
+s.connect((host,port))
+recieve_thread = Thread(target=recieve)
+recieve_thread.start()
+
 mainloop()
