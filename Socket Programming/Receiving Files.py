d1ba92e619bdd91b59caca164b6b645640b0d10b
vijay <vijaybalajitheboss@gmail.com>
2020-05-18 14:16:04 +0530

    Made both the files run through cmd

--- Socket Programming/Receiving Files.py
+++ Socket Programming/Receiving Files.py
@@ -1,14 +1,16 @@
 import socket
-
+print("Run the Server and the Client through command prompt using \"python filename.py\"",
+"from the Socket Programming directory")
 s = socket.socket()
 host = "localhost"; port = 8080
 s.connect((host,port))
-path = "Socket Programming/assets/"
+path = "assets/"
 x = input("Enter the name of the file you want to read : ")
 fileName = (path+x)
 s.send(fileName.encode())
 info = s.recv(1024)     # Getting the info from the file.
 print(info.decode())
 s.close()
+input("Press any key to exit ")
 
  
