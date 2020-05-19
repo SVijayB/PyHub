4ecf2a9bda00f7681c226b936be32f2baaad12ee
vijay <vijaybalajitheboss@gmail.com>
2020-05-20 01:45:30 +0530

    Updated Code

--- Databases using SQLite3/Creating Attributes.py
+++ Databases using SQLite3/Creating Attributes.py
@@ -2,6 +2,7 @@ import sqlite3
 # In RDBMS, Attributes are columns of the table.
 database = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
 print("Connection has been made successfuly!")
-database.execute("""CREATE TABLE Student_Records(ID INT PRIMARY KEY NOT NULL, NAMES TEXT NOT NULL, MARKS TEXT NOT NULL, GRADES NOT NULL)""")
+database.execute("""CREATE TABLE Student_Records(ID INT PRIMARY KEY NOT NULL, 
+NAMES TEXT NOT NULL, MARKS TEXT NOT NULL, GRADES NOT NULL)""")
 print("Columns created in your Database")
 database.close()
