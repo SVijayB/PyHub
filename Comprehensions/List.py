6a67b7fc01e816d165ad76b168a186999a55d98a
vijay <vijaybalajitheboss@gmail.com>
2020-05-19 12:02:04 +0530

    Added List Comprehension

---
+++ Comprehensions/List.py
@@ -0,0 +1,18 @@
+print("List comprehension is an elegant way to define and create lists based on existing lists.",
+"list comprehensions are usually more human readable than lambda functions.",
+"It is easier to understand what the programmer was trying to accomplish when list comprehensions are used")
+# Syntax :
+#[expression (variable) for variable in input_set [predicate][,...]]
+
+print("Example 1 : ")
+x = set(i for i in range(10) if i%2==0)
+print(x)
+
+print("Example 2 : ")
+list1 = [1,2,3,4,5,6,7,8,9,10]
+x = set(i for i in list1 if i%2==0)
+print(x)
+
+print("Example 3 : ")
+x = set((a,b) for a in range(3) for b in range(4))
+print(x)  
