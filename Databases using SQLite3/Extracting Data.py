8ff7e478e11c7257ca235fcfe465f5dd5fad1814
vijay <vijaybalajitheboss@gmail.com>
2020-05-24 22:19:56 +0530

    Printing multiple records using fetchone

--- Databases using SQLite3/Extracting Data.py
+++ Databases using SQLite3/Extracting Data.py
@@ -5,9 +5,7 @@ cursor = connection.cursor()
 print("Printing all records")
 query = """SELECT NAMES,MARKS FROM Student_Records"""   # Select * For all attributes
 cursor.execute(query)
-
 student_record = cursor.fetchall()
-
 for names,marks in student_record:
     print(names,marks)
 connection.close()
@@ -15,10 +13,21 @@ connection.close()
 print("\nPrinting a single record using fetchone")
 connection = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
 cursor = connection.cursor()
-
 query = """SELECT NAMES,MARKS FROM Student_Records""" 
 cursor.execute(query)
-
 student_record = cursor.fetchone()
-
 print(student_record)
+
+print("\nPrinting multiple records using fetchone")
+connection = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
+cursor = connection.cursor()
+query = """SELECT NAMES,MARKS FROM Student_Records""" 
+cursor.execute(query)
+student_record = cursor.fetchone()
+while True:
+    student_record = cursor.fetchone()
+    if (student_record==None):
+        break
+    print(student_record)
+print("\nNotice that it prints from the 2nd record, this is because we already printed the first one a while ago.",
+"thus, the cursor points to the very next record.")
