998a9caaaa31503cf77be99fde6b056f2adb39f4
vijay <vijaybalajitheboss@gmail.com>
2020-05-24 22:15:57 +0530

    Added fetchone command

--- Databases using SQLite3/Extracting Data.py
+++ Databases using SQLite3/Extracting Data.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
 cursor = connection.cursor()
-
+print("Printing all records")
 query = """SELECT NAMES,MARKS FROM Student_Records"""   # Select * For all attributes
 cursor.execute(query)
 
@@ -10,3 +10,15 @@ student_record = cursor.fetchall()
 
 for names,marks in student_record:
     print(names,marks)
+connection.close()
+
+print("\nPrinting a single record using fetchone")
+connection = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
+cursor = connection.cursor()
+
+query = """SELECT NAMES,MARKS FROM Student_Records""" 
+cursor.execute(query)
+
+student_record = cursor.fetchone()
+
+print(student_record)
