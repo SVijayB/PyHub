ac59b13547587c467ed95c25faaae0fdff24c5fa
vijay <vijaybalajitheboss@gmail.com>
2020-05-25 17:52:44 +0530

    Added where clause

--- Databases using SQLite3/Where Clause.py
+++ Databases using SQLite3/Where Clause.py
@@ -1,1 +1,13 @@
 # Where clause is used to get values which maches reqests
+import sqlite3
+
+Connection = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
+print("Connection has been made successfuly!")
+cursor = Connection.cursor()
+query = """SELECT NAMES,MARKS FROM Student_Records WHERE MARKS>50 AND ID<2""" # AND AND OR CAN BE USED
+
+cursor.execute(query)
+records = cursor.fetchall()
+
+for x,y in records:
+    print(x,y)
