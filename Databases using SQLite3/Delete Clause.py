f1051401b6fe033d07eed59aeb909a86eb56517b
vijay <vijaybalajitheboss@gmail.com>
2020-05-26 13:03:57 +0530

    Added deleting using like clause

--- Databases using SQLite3/Delete Clause.py
+++ Databases using SQLite3/Delete Clause.py
@@ -12,3 +12,15 @@ student_record = cursor.fetchall()
 for id,names,marks,grades in student_record:
     print(id,names,marks,grades)
 
+print("\nDELETING RECORDS WHERE THE NAMES START WITH S")
+db = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
+query = """DELETE FROM Student_Records WHERE NAMES LIKE 's%'"""
+cursor = db.cursor()
+cursor.execute(query)
+db.commit()
+query = """SELECT * FROM Student_Records ORDER BY NAMES ASC"""   # Select * For all attributes
+cursor.execute(query)
+student_record = cursor.fetchall()
+for id,names,marks,grades in student_record:
+    print(id,names,marks,grades)
+db.close()
