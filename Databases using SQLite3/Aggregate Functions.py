219f8f1e78f1cd2d43d584e02a4fb318a6933696
vijay <vijaybalajitheboss@gmail.com>
2020-06-01 15:59:11 +0530

    Added agregate functions

---
+++ Databases using SQLite3/Aggregate Functions.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def fun(query):
+    cursor.execute(query)
+    foo = cursor.fetchall()
+    for x in foo:
+        print(x)
+    print()
+
+db = sqlite3.connect("Databases using SQLite3/assets/Student_Records.db")
+cursor = db.cursor()
+
+query = """SELECT AVG(MARKS) FROM Student_records"""
+fun(query)
+query2 = """SELECT AVG(MARKS) FROM Student_records WHERE ID<7""" # Average of first 6 students
+fun(query2)
+query3 = """SELECT MAX(MARKS) FROM Student_records"""
+fun(query3)
+query4 = """SELECT COUNT(*) FROM STUDENT_records""" # Number of records
+fun(query4)
+query4 = """SELECT COUNT(DISTINCT MARKS) FROM STUDENT_records""" # Prints number of disctinct marks
+fun(query4)
+query4 = """SELECT COUNT(NAMES) FROM STUDENT_records WHERE NAMES LIKE 'a%'""" # Number of name starting with a
+fun(query4)
+query3 = """SELECT SUM(MARKS) FROM Student_records""" # Finds sum of all marks
+fun(query3)
