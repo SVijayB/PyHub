20a92e977eff8c0043997f6edd3e902adc662fbd
vijay <vijaybalajitheboss@gmail.com>
2020-06-03 17:57:52 +0530

    Added an example for yield statement

--- Generators/Yield Statement.py
+++ Generators/Yield Statement.py
@@ -19,3 +19,38 @@ def x():
 y = x()
 for i in range(3):
     print(y.__next__())
+
+"""
+print("\nExample 2 : ")
+print("Without Using Yield Statemenet")
+def calls_recieved(recieved):
+    a = 0
+    while(True):
+        if(recieved=="yes"):
+            a = a+1
+            return a
+        else:
+            return a
+
+while (True):
+    recieved = input("Did you recieve a call or not? : ")
+    total = calls_recieved(recieved)
+    print(total)   
+"""
+
+print("\nUsing the Yield Statemenet")
+def calls_recieved(recieved=None):
+    a = 0
+    while (True):
+        if recieved == "yes":
+            a = a+1
+            recieved = yield a
+        else:
+            recieved = yield a
+
+rec = calls_recieved()
+rec.__next__()
+       
+while (True):
+    ans = input("Did you recieve a call or not? : ")
+    print(str(rec.send(ans)))  
