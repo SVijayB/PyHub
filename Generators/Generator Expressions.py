dfa3cd79320dfdf91bd8598d2d5b627133af9a2d
vijay <vijaybalajitheboss@gmail.com>
2020-06-03 18:15:14 +0530

    Added generator expressions

---
+++ Generators/Generator Expressions.py
@@ -0,0 +1,11 @@
+# You can create a generator using a generator expression like a lambda function.
+# This function does not need or use a yield keyword.
+# Syntax : Y = ([ Expression ])
+
+y = [1,2,3,4,5]
+
+print([x**2 for x in y])
+
+print("\nDoing this without using the generator expressions :")
+length = len(y)
+print((x**2 for x in y).__next__())
