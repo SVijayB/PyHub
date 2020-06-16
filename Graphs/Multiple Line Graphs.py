9c8e21808337ed57b9686b58b48de107c77d81ce
vijay <vijaybalajitheboss@gmail.com>
2020-06-16 10:21:36 +0530

    Added Multiple Line graphs for comparisons

---
+++ Graphs/Multiple Line Graphs.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as graphs
+
+Halflife = [10,20,30,40,50,60,70,80,90,100]
+Halflife2 = [10,20,30,40,50,60,70,80,90,100]
+
+Halflife_difficulty = [10,25,34,17,48,56,67,54,87,64]
+Halflife2_difficulty = [5,15,27,54,62,65,47,84,68,48]
+
+graphs.xlabel("Game Progress %")
+graphs.ylabel("Game Difficulty")
+graphs.title("Half Life and Half Life 2 Difficulty Comparison")
+
+graphs.plot(Halflife,Halflife_difficulty,color='y')
+graphs.plot(Halflife2,Halflife2_difficulty,color='b')
+graphs.show()
