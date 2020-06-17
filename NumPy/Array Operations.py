747644c4d5d7455ed6b4dd9f1717192bdd5f71d1
vijay <vijaybalajitheboss@gmail.com>
2020-06-17 15:30:56 +0530

    Converted comments into print statements

--- NumPy/Array Operations.py
+++ NumPy/Array Operations.py
@@ -1,26 +1,41 @@
 import numpy as np
 
-a = np.array([3,1,9,1])     # One Dimentional array
+a = np.array([3,1,9,1,5,3,7,8,9,5])     # One Dimentional array
 b = np.array([[1,7,3,4],[9,3,6,1],[9,2,8,1]])   # Two Dimentional array
-print(a)
-print(b)
+print("a :" ,a)
+print("b :", b)
 
-# Accessing a certain item.
-# 1D
+print("\nAccessing a certain item.")
+print("1D : ")
 print(a[2])     # Starts with 0
 print(a[-1])    # Prints last element
-# 2D
+print("2D : ")
 print(b[1][2])
 
-# Finding the dimension :
+print("\nFinding the dimension :")
 print(a.ndim,"Dimensional array")
 print(b.ndim,"Dimensional array")
 
-# Shape (Rows, Column)
+print("\nShape (Rows, Column)")
 print(b.shape)
 
-# Size(Number of elements)
+print("\nSize(Number of elements)")
 print(b.size)
 
-# Datatype :
+print("\nDatatype :")
 print(a.dtype)
+
+print("\nUpdating an array :")
+print("1D : ")
+print(a)
+a[1] = 6
+print(a)
+print("2D :")
+print(b)
+b[1][2] = 8
+print(b)
+
+print("\nPrinting only fixed values :")
+print(a[:3])
+print(a[3:])
+print(a[3:8])
