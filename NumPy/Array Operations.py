f65292f95a06543a747eb137b056fd204142e89d
vijay <vijaybalajitheboss@gmail.com>
2020-06-17 15:46:36 +0530

    Added in more numpy functions

--- NumPy/Array Operations.py
+++ NumPy/Array Operations.py
@@ -35,7 +35,43 @@ print(b)
 b[1][2] = 8
 print(b)
 
-print("\nPrinting only fixed values :")
+print("\nPrinting only fixed values(Read comments) : ")
+print("1D : ")
+print("a :" ,a)
 print(a[:3])
 print(a[3:])
 print(a[3:8])
+print(a[::2])   # Skips a value (prints all the second terms)
+print(a[::3])   # Prints only the third value
+print(a[2::3])  # Starts from the second step and prints only the third term from then
+print(a[8::-2]) # Prints from the reverse direction
+
+print("2D : ")
+print("b :\n", b)
+print(b[:2,:1])
+print(b[:2,:2])
+print(b[0,:])   # Prints only the first row
+print(b[:,0])   # Prints only the first column
+
+print("\nData Conversions : ")
+c = np.array([[1,7,6,4],[2,8.4,7,11],[2,6,4.3,12]],dtype = int)
+print("Int : \n",c)
+c = np.array([[1,7,6,4],[2,8.4,7,11],[2,6,4.3,12]],dtype = float)
+print("Float : \n",c)
+
+print("\n Null Matrix : ")
+c = np.zeros([3,4],dtype = int) # By default, values are in float
+print(c)
+
+print("\nUnit Matrix : ")
+c = np.ones([3,4],dtype = int)
+print(c)
+
+print("\nCreating an array with a sequence of numbers :")
+c = np.arange(5,70,4)
+print(c)
+x = 2*((4*2)-1)        
+c = np.arange(5,70,x)  # You can use complex equations as well
+print(c)
+c = np.arange(5,70,0.5) # You can use floating point values as well
+print(c)
