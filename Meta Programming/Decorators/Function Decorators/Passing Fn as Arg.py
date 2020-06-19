2c91973014784b5328f05213b5e227fe06680cd9
vijay <vijaybalajitheboss@gmail.com>
2020-06-19 12:43:12 +0530

    Completed Function decorators

---
+++ Meta Programming/Decorators/Function Decorators/Passing Fn as Arg.py
@@ -0,0 +1,10 @@
+# Pass a function as an argumenet to another function.
+def x(func):
+    print("Hey I am the function that has a function as argument!")
+
+def y():
+    print("Hey! I am the function being passed as an argument to another function")
+
+x(y())  # Argument function is executed first and then the calling function.
+
+input("Press any key to exit ")
