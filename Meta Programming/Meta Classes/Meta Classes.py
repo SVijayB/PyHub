33eaa1e95fbbc392942330484a0305a2bce08d06
vijay <vijaybalajitheboss@gmail.com>
2020-06-19 13:53:53 +0530

    Added in meta classes and completed meta programming

---
+++ Meta Programming/Meta Classes/Meta Classes.py
@@ -0,0 +1,15 @@
+# The instance of a class is called a meta class.
+def metafunction():
+    print("This is the metaclass function")
+
+class inherit():
+    def func(self):
+        print("This is the inherited method")
+    
+metaobject = type('meta', (inherit, ),dict(name = "Vijay", metafunction=metafunction))
+print(type(metaobject))
+b = metaobject()
+print(type(b))
+
+b.func()
+print(b.name)
