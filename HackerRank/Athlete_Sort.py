5c5a0e3d65ce9c09dad3da23ec75bc18f852c3cc
vijay <vijaybalajitheboss@gmail.com>
2020-07-02 17:41:17 +0530

    Removed Hackerrank files

--- HackerRank/Athlete_Sort.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    n, m = map(int, input().split())
-    arr = [input() for i in range(n)]
-    k = int(input())
-
-    for ans in sorted(arr, key = lambda ans: int(ans.split()[k])):
-        print(ans)
-input("Press any key to exit ")
--- HackerRank/Division.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a//b) #Floor Division
-    print(a/b)  #Float Division
-input("Press any key to exit ")
