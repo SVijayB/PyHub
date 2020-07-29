98eb1cf56917900cbbd4b10316f0d8e2966259f0
vijay <vijaybalajitheboss@gmail.com>
2020-07-29 15:36:34 +0530

    Added in list appender for readme files

---
+++ Miscellaneous/ReadMe List.py
@@ -0,0 +1,42 @@
+"""
+Program description : 
+Helps you in creating an easy README interface with list of data and programs linked to each other.
+"""
+def Concat(n, base_url):
+    para = ""
+    base_url_length = len(base_url)
+    for i in range(n):
+        url = input("Enter the URL of the program \n> ")
+        url_length = len(url)
+        name = ""
+        j = base_url_length + 1
+        while(j<url_length):
+            name = name + url[j]
+            j = j+1
+        name = name.replace("%20"," ")
+        
+        point = "• "
+        start_tag = ("<a href=\""+url+"\">")
+        end_tag = ("</a>")
+        link = start_tag+name+end_tag
+        space_length = 30 - len(name)
+        space = ""
+        
+        for i in range(space_length):
+            space = space + " "
+
+        message = input("Enter Your message here \n> ")
+        message = ("| " + message)
+        end = (".")
+        final_result = point+link+space+message+end
+
+        print(final_result)
+
+        para = (para + final_result+ "\n")
+    
+    print(para)
+
+if __name__ == "__main__":
+    n = int(input(("Enter number of entries \n> ")))
+    base_url = input("Enter Base URL \n> ")
+    Concat(n, base_url)
