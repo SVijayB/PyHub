f2fb6494fb401a6cea8fe16cc215083b0c378b07
vijay <vijaybalajitheboss@gmail.com>
2020-07-31 02:33:17 +0530

    Enabled the readme list to copy final result to clipboard

--- Miscellaneous/ReadMe List.py
+++ Miscellaneous/ReadMe List.py
@@ -1,6 +1,9 @@
+import pyperclip
 """
 Program description : 
 Helps you in creating an easy README interface with list of data and programs linked to each other.
+
+Note : To run this program -> `pip install pyperclip` first.
 """
 def Concat(n, base_url):
     para = ""
@@ -22,7 +25,7 @@ def Concat(n, base_url):
         space_length = 30 - len(name)
         space = ""
         
-        for i in range(space_length):
+        for x in range(space_length):
             space = space + " "
 
         message = input("Enter Your message here \n> ")
@@ -32,9 +35,11 @@ def Concat(n, base_url):
 
         print(final_result)
 
-        para = (para + final_result+ "\n")
+        para = ("\n" + para + final_result + "\n")
     
     print(para)
+    pyperclip.copy(para)
+    print("The final result has been copied to your clipboard :)")
 
 if __name__ == "__main__":
     n = int(input(("Enter number of entries \n> ")))
