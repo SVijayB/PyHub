15d942b16d971a1d624175fc6ba6498211eb9db6
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2020-10-01 00:29:42 +0530

    Added multiple variable assignments
    Python supports multiple variable assignments on a single line!

--- Basics/Variables.py
+++ Basics/Variables.py
@@ -1,11 +1,15 @@
 # No specific command to declare a variable.
 
-x = 5           # Automatically assigned when given a value.
+x = 5                                             # Automatically assigned when given a value.
 print(x)
-y = 57.15       # Datatype need not be mentioned while initializing.
+y = 57.15                                         # Datatype need not be mentioned while initializing.
 print(y)
-x = 54.3        # Data is overwritten if you reassign a new value to the variable.
+x = 54.3                                          # Data is overwritten if you reassign a new value to the variable.
 print(x)
-X = 52.7        # Variable names are case sensitive.
+X = 52.7                                          # Variable names are case sensitive.
 print(X)
+x, y, z = "Orange", "Banana", "Cherry"            # Python allows you to assign values to multiple variables in one line
+print(x)
+print(y)
+print(z)
 input("Press any key to exit ")
