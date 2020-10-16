62e21b618a74652176dfb4ec665959990e759496
vijay <vijaybalajitheboss@gmail.com>
2020-10-16 20:38:26 +0530

    Removed a few programs

--- Statistics/GridChallenge.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-for _ in range(n):
-    arr=(sorted(input()) for _ in range((int(input()))))
-    idx=(True if (list(z)==sorted(z)) else False for z in zip(*arr))
-    print(['NO','YES'][all(idx)])
-
