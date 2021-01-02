726302bf47cb45de2f7937571c7d52b5c63bbf6f
vijay <vijaybalajitheboss@gmail.com>
2021-01-02 20:18:53 +0530

    Completed transitive

--- Miscellaneous/Relations.py
+++ Miscellaneous/Relations.py
@@ -1,13 +1,13 @@
 def reflexive(numbers):
-    print("{",end = " ")
+    print("REFLEXIVE = [{",end = "\n  ")
     for i in numbers:
         for j in numbers:
             if(i==j):
-                print("(" + str(i) + "," + str(j) + ")", end=" ")
-    print("}")
+                print("(" + str(i) + "," + str(j) + ")", end="\n  ")
+    print("}]")
 
 def symmetric(numbers):
-    print("[",end = "\n")
+    print("SYMMETRIC = [",end = "\n")
     for i in range(0, len(numbers)):
         for j in range(i+1, len(numbers)):
             print("  {",end = " ")
@@ -17,7 +17,16 @@ def symmetric(numbers):
     print("]")
 
 def transitive(numbers):
-    pass
+    print("TRANSITIVE = [",end = "\n")
+    for i in range(0, len(numbers)):
+        for j in range(i+1, len(numbers)):
+            for k in range(j+1, len(numbers)):
+                print("  {",end = " ")
+                print("(" + str(numbers[i]) + "," + str(numbers[j]) + ")", end=" ")
+                print("(" + str(numbers[j]) + "," + str(numbers[k]) + ")", end=" ")
+                print("(" + str(numbers[i]) + "," + str(numbers[k]) + ")", end=" ")
+                print("}")
+    print("]")
 
 if __name__ == "__main__":
     n = int(input("How many elements in the set?\n> "))
@@ -29,6 +38,6 @@ if __name__ == "__main__":
 
     numbers_set = set(numbers)
     numbers_set = list(numbers_set)
-    # reflexive(numbers_set)
-    # symmetric(numbers_set)
+    reflexive(numbers_set)
+    symmetric(numbers_set)
     transitive(numbers_set)
