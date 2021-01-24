252b408764a37d1768b5100e47641a7e2b7baf47
vijay <vijaybalajitheboss@gmail.com>
2021-01-25 00:13:29 +0530

    Added local time manipulation to basics

---
+++ Basics/Time.py
@@ -0,0 +1,24 @@
+# Accessing and manipulating local time.
+import time
+
+seconds = time.time()
+local_time = time.ctime(seconds)
+print("Local time:", local_time)
+
+x = time.struct_time(tm_year=2021, tm_mon=1, tm_mday=31, 
+                    tm_hour=9, tm_min=28, tm_sec=56, 
+                    tm_wday=6, tm_yday=31, tm_isdst=0)
+
+seconds = time.time()
+curr_time = time.localtime(seconds)
+print(curr_time)
+print("Current year -> ", curr_time.tm_year)
+
+example = "17 July 2001"
+ans = time.strptime(example, "%d %B %Y")
+print(ans)
+
+import time
+for i in range (1,6):
+    print(i)
+    time.sleep(1)
