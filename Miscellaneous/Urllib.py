c7ac684a156a83b53d29d353a5283e551003baf2
vijay <vijaybalajitheboss@gmail.com>
2021-02-02 19:37:44 +0530

    Simple program to extract data using request

---
+++ Miscellaneous/Urllib.py
@@ -0,0 +1,8 @@
+import urllib.request 
+
+data = urllib.request.urlopen('https://official-joke-api.appspot.com/random_ten') 
+data = data.read().decode()
+print(data) 
+file = open("content.txt", "w+")
+file.write(data)
+file.close()
