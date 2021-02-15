4732a4c2654e204f87c940771a8f58caefee740c
vijay <vijaybalajitheboss@gmail.com>
2021-02-15 23:02:52 +0530

    Extract text data from pdf

---
+++ Miscellaneous/PDFMiner.py
@@ -0,0 +1,26 @@
+import io
+
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = file_handle.getvalue()
+    converter.close()
+    file_handle.close()
+
+    return text
+
+if __name__ == '__main__':
+    data = extract_text_from_pdf('Hello-world.pdf')
+    print(data)
