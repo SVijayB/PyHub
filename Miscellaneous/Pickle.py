62fea448c58479715ddbc43a59e897c7dad10793
vijay <vijaybalajitheboss@gmail.com>
2021-02-23 19:30:12 +0530

    saving and extracting data from a pickle file

---
+++ Miscellaneous/Pickle.py
@@ -0,0 +1,16 @@
+import pickle 
+
+def store():
+    dict_data = {"Name":"Vijay","Age":"20","Gender":"Male"}
+    file = open("Pickle.pkl","wb")          # For pickle files use WB - Write and read from a binary file.
+    pickle.dump(dict_data,file)
+    file.close()
+  
+def load(): 
+    with open("assets/Pickle.pkl", "rb") as fp:
+        data = pickle.load(fp)
+    print(data)
+  
+if __name__ == '__main__': 
+    store() 
+    load() 
