20177613ff262102fc13352bab5dbc12690f7388
vijay <vijaybalajitheboss@gmail.com>
2021-02-26 22:32:06 +0530

    Added methods extracted from pendulum.

--- Miscellaneous/Date and time.py
+++ Miscellaneous/Date and time.py
@@ -1,13 +1,39 @@
 from datetime import *
-from dateutil import parser
+from dateutil import *
 from dateutil.relativedelta import *
 from dateutil.parser import * 
+import pendulum
 
-print (parser.parse('Friday, 26. February 2021 05:52PM')) 
-print (parser.parse('2 / 26 / 2021 05:52:26')) 
-print (parser.parse('2021-02-26T10:52:26Z'))
 
 print("Right now it is : ", datetime.now())
 print("The Date today is : ", date.today())
 print(datetime.now() + relativedelta(months=+1))
 print(datetime.now() + relativedelta(years = 17,months=+1, weeks=+1))
+
+dt = pendulum.datetime(2021, 2, 25)
+print(isinstance(dt, datetime))
+pendulum.datetime(2020, 2, 25, tz='Europe/Paris')
+dt = pendulum.local(2020,2, 25)
+now_in_london = pendulum.now('Europe/London')
+print(now_in_london.timezone_name)
+
+print(pendulum.now())
+print(pendulum.today())
+print(pendulum.yesterday())
+print(print(pendulum.tomorrow()))
+
+print(pendulum.parse('2020-02-25T21:00:00'))
+print(pendulum.parse('31-01-01', strict=False))
+
+first = pendulum.datetime(2012, 9, 5, 23, 26, 11, 0, tz='America/Toronto')
+second = pendulum.datetime(2012, 9, 5, 20, 26, 11, 0, tz='America/Vancouver')
+print(first == second)
+print (first > second)
+print(first <= second)
+
+dt = pendulum.datetime(2021, 2, 26)
+print(dt)
+dt = dt.add(years=5)
+print(dt)
+dt = dt.subtract(years=1)
+print(dt)
