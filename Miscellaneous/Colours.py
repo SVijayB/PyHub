1f82ed4d1884164b31fb04f960e824d54a9355ec
vijay <vijaybalajitheboss@gmail.com>
2021-02-28 16:16:09 +0530

    Added a program that would change console output colour

---
+++ Miscellaneous/Colours.py
@@ -0,0 +1,30 @@
+black = "\033[1;0;40m"
+
+def red(message):
+    print("\033[1;31;40m"+ message + black)
+
+def grey(message):
+    print("\033[1;30;40m" + message + black)
+
+def green(message):
+    print("\033[1;32;40m" + message + black)
+
+def yellow(message):
+    print("\033[1;33;40m" + message + black)
+
+def blue(message):
+    print("\033[1;34;40m" + message + black)
+
+def pink(message):
+    print("\033[1;35;40m" + message + black)
+
+def cyan(message):
+    print("\033[1;36;40m" + message + black)
+
+red("Hello!")
+grey("World!")
+green("How")
+yellow("You")
+blue("Doin!")
+pink(":D")
+cyan("Adios!!")
