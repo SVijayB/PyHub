0fcb35a166a578b79b7decd96c5105979b804847
vijay <vijaybalajitheboss@gmail.com>
2021-04-19 20:35:38 +0530

    Added a program to spam requests to any page

---
+++ Miscellaneous/RequestSpammer.py
@@ -0,0 +1,18 @@
+# Spam rogue requests to a webpage to test how well it can handle multiple requests. Add in more threads for faster testing.
+
+from selenium import webdriver
+import time
+from threading import Thread
+
+def one():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-notifications')
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(executable_path="D:\chromedriver",options=chrome_options)
+
+    for i in range(200):
+        driver.get("https://www.google.com/")
+    print("Done")
+
+t1 = Thread(target=one).start()
+t2 = Thread(target=one).start()
