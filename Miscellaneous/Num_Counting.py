37f20f8ac2d76cf9a32ce542ba0398555caf6e0d
vijay <vijaybalajitheboss@gmail.com>
2021-10-16 23:18:52 +0530

    Reformated code

--- Miscellaneous/Num_Counting.py
+++ Miscellaneous/Num_Counting.py
@@ -3,17 +3,19 @@
 import pyautogui
 import time
 
-def counter(num,end):
+
+def counter(num, end):
     time.sleep(5)
-    for i in range (num, end):
+    for i in range(num, end):
         pyautogui.typewrite(str(i))
-        pyautogui.press('enter')
-        pyautogui.hotkey('alt', 'tab')
+        pyautogui.press("enter")
+        pyautogui.hotkey("alt", "tab")
         i += 1
         time.sleep(1)
 
+
 if __name__ == "__main__":
-    num = (int(input("Enter current number: ")) + 1) 
+    num = int(input("Enter current number: ")) + 1
     end = int(input("Enter final number: "))
-    counter(num,end)
+    counter(num, end)
     print("Done!")
