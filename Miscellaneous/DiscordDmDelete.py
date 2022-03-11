c8e9028ce994f8f9d9a1d88d13c9bbf230e09227
vijay <vijaybalajitheboss@gmail.com>
2022-03-11 14:43:44 +0530

    Completed auto message deleting script. Efficiency is trash though xD

--- Miscellaneous/DiscordDmDelete.py
+++ Miscellaneous/DiscordDmDelete.py
@@ -1,11 +1,25 @@
 # Python script to automate deleting Discord DM messages.
+import pyautogui
+import time
 
 
 def AutoDelete(msgCount):
     for i in range(msgCount):
-        pass
+        pyautogui.press("up")
+        pyautogui.hotkey("ctrl", "a")
+        pyautogui.hotkey("backspace")
+        pyautogui.press("enter")
+        pyautogui.press("enter")
+        pyautogui.click(500, 976)
+        time.sleep(0.1)
+
+
+def Get_position():
+    print(pyautogui.position())
 
 
 if __name__ == "__main__":
-    msgCount = input("How many messages would you like to delete? > ")
+    msgCount = int(input("How many messages would you like to delete? > "))
+    # Get_position()
+    time.sleep(2)
     AutoDelete(msgCount)
