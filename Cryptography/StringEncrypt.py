323f79c1220c09ac4203e858f9a377bc497630b5
jambathehood <72135274+jambathehood@users.noreply.github.com>
2022-10-10 00:13:33 +0530

    Updated to add string encryption using RSA module

--- Cryptography/StringEncrypt.py
+++ Cryptography/StringEncrypt.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import rsa
 
 message = "Hello world!"
 key = Fernet.generate_key()
@@ -10,4 +11,15 @@ print("Encrypted string: ", encMessage)
 
 decMessage = fernet.decrypt(encMessage).decode()
  
+print("Decrypted string: ", decMessage)
+
+# --- RSA MODULE ---
+
+publicKey, privateKey = rsa.newkeys(512)
+
+encMessage = rsa.encrypt(message.encode(), publicKey)
+ 
+print("Original string: ", message)
+print("Encrypted string: ", encMessage)
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
 print("Decrypted string: ", decMessage)
